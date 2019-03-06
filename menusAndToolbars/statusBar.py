aca6f652c44a88104c2c692d97bc12ec88763322
RollingBear <zhangshuo1120@hotmail.com>
2019-03-06 17:37:15 +0800

    update code

---
+++ menusAndToolbars/statusBar.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+#   2019/3/6 0006 上午 9:59     
+
+__author__ = 'RollingBear'
+
+
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Example(QMainWindow):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.statusBar().showMessage('Ready')
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Statusbar')
+
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
